7ad8d4e33a6fa973a8b97539184c0107b62d73f0
FredMartins <frederico.martins@edu.pucrs.br>
2023-04-05 16:19:50 -0300

    Adicionados controladores do sistema

---
+++ src/main/java/org/springframework/desafio/db/sistema/ControladorBoasVindas.java
@@ -0,0 +1,13 @@
+package org.springframework.desafio.db.sistema;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorBoasVindas {
+
+    @GetMapping("/")
+    public String boasVindas(){
+        return "boasVindas";
+    }
+}
---
+++ src/main/java/org/springframework/desafio/db/sistema/ControladorCrash.java
@@ -0,0 +1,12 @@
+package org.springframework.desafio.db.sistema;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorCrash {
+    @GetMapping("/oups")
+    public String dispararExcessao(){
+        throw new RuntimeException("Algo de errado ocorreu! Por favor, tente novamente mais tarde!");
+    }
+}
