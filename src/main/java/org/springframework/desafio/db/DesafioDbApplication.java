2e1e7e55a6d430f95845c880869665c2a25cf266
FredMartins <frederico.martins@edu.pucrs.br>
2023-04-06 13:56:14 -0300

    Classe de ativação da aplicação adicionada

---
+++ src/main/java/org/springframework/desafio/db/DesafioDbApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.desafio.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class DesafioDbApplication {
+
+    public static void main(String[]args){
+        SpringApplication.run(DesafioDbApplication.class,args);
+    }
+}
