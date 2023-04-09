51ff927bee6f226f6994001b9da02e3c0e654568
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2023-04-09 19:04:51 -0300

    Adicionadas fontes, arquivos de css e sql ao projeto

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO dbservantes VALUES (default, 'Frederico', 'Martins', 'Rocket', null);
+INSERT INTO dbservantes VALUES (default, 'Renato', 'Chitolina', 'Rocket', null);
+INSERT INTO dbservantes VALUES (default, 'Jane', 'Eslabao', 'Rocket', null);
+INSERT INTO dbservantes VALUES (default, 'Juliana', 'Dias', 'Rocket', null);
+
+INSERT INTO restaurantes VALUES (default, 'Palatus',0);
+INSERT INTO restaurantes VALUES (default, 'Panorama', 0);
+INSERT INTO restaurantes VALUES (default, 'Silva Lanches', 0);
+
+INSERT INTO votacao VALUES (default, 1, '2023-04-07', 'sexta-feira', '10:30',3);
+INSERT INTO votacao VALUES (default, 2, '2023-04-06', 'quinta-feira', '10:25',2);
